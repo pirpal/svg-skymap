e259593a0a370ac62535d8c87ba9b489a8f7ea28
pirpal <peyop78@gmail.com>
2019-03-10 22:35:03 +0100

    kivy minimal ok

---
+++ lib/drawable_star.py
@@ -0,0 +1,6 @@
+#!usr/bin/env python3
+# coding: utf8
+
+class DrawableStar:
+    def __init__(self):
+        pass
--- main.py
+++ main.py
@@ -3,13 +3,42 @@
 
 try:
     from kivy.app import App
+    from kivy.graphics import *
+    from kivy.graphics.context_instructions import Color
+    from kivy.uix.widget import Widget
     from kivy.uix.button import Button
+    from kivy.uix.label import Label
+    from kivy.uix.gridlayout import GridLayout
 except ModuleNotFoundError as e:
-    print(f"!e> {e}")
+    print(e)
+
+
+class SkyMapCanvas(Widget):
+    def __init__(self, **kwargs):
+        super(SkyMapCanvas, self).__init__(**kwargs)
+        with self.canvas:
+            # add your instruction for main canvas here
+            Color(1, 0.8, .4, mode='rgb')
+            Rectangle(pos=(10, 10), size=(50, 50))
+            Rectangle(pos=(100, 30), size=(20, 20))
+
+        with self.canvas.before:
+            # you can use this to add instructions rendered before
+            pass
+        with self.canvas.after:
+            # you can use this to add instructions rendered after
+            pass
+
+
+    def drawConstellation():
+        pass
 
 class TestApp(App):
     def build(self):
-        return Button(text="Hello World!")
+        layout = GridLayout(cols=2)
+        canvas = SkyMapCanvas()
+        layout.add_widget(canvas)
+        return layout
 
 
 if __name__ == "__main__":
